690871178a6107ea286e234e73240ce6819634cd
gyeongjun-web <wnsdign@naver.com>
2025-07-04 16:12:13 +0900

    jesus
    Please enter the commit message for your changes. Lines starting

--- python/ex10.py
+++ python/ex10.py
@@ -1,1 +1,10 @@
-print("hello world")
+temp=int(input('현재온도'))
+if 120>temp>=25:
+    print('반바지 추천')
+elif temp<25:
+    print('긴바지 추천')
+elif temp>120:
+    print('자살하시오')
+
+else:
+    print('잘못된 입력')
