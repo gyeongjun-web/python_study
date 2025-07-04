5941aae8a1c894a32efc7c277622130f58b8a236
gyeongjun-web <wnsdign@naver.com>
2025-07-04 16:45:03 +0900

    oh yeah

---
+++ python/ex12.py
@@ -0,0 +1,11 @@
+a=input('하나의 문장 입력:')
+
+if a=='R' or a=='r':
+    print('Rectangle')
+elif a=='T' or  a=='t':
+    print('Triangle')
+elif a=='C' or a=='c':
+    print('Circle')
+
+else:
+    print('Unknown')
